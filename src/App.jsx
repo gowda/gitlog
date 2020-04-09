d8356016e5d293d9ea1e1c060f1eb55e57989fe9
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-04-09 21:19:25 +0200

    React application tooling adapted from scratch
    https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <h1>ನಮಸ್ಕಾರ ಗುರೂ!!</h1>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App);
