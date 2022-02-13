1f90b0ba0529a72e22eb8e1a55487198b9384e1e
gowda <gowda@users.noreply.github.com>
2022-02-13 16:03:28 +0000

    Update gh-pages on master changes

--- src/components/scroll.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default React.forwardRef<HTMLDivElement>((_props, ref) => (
-  <div ref={ref} id='scroll' className='invisible'>
-    Scroll placeholder
-  </div>
-));
